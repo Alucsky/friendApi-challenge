1fe7e904724e2cda0f018a19fbe36d8bcda9f19f
Alucsky <samuelmolinari8@gmail.com>
2025-03-17 14:35:32 -0300

    mapping routes in app

--- src/HTTP/controllers/routes/pet.routes.ts
+++ src/HTTP/controllers/routes/pet.routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { RegisterPet } from '../pets/registerPet'
 import { SearchEspecifyPet } from '../pets/searchEspecifyPet'
 
-export async function OrgRoutes(app: FastifyInstance) {
+export async function PetRoutes(app: FastifyInstance) {
   app.post('/pet', RegisterPet)
   app.get('/pet/:id', SearchEspecifyPet)
   app.get('/pet', SearchEspecifyPet)
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import { orgRoutes } from '@/HTTP/controllers/routes/org.routes'
-import { petRoutes } from '@/HTTP/controllers/routes/pet.routes'
-
 import fastify from 'fastify'
+import { OrgRoutes } from './HTTP/controllers/routes/org.routes'
+import { CitiesRoutes } from './HTTP/controllers/routes/city.routes'
+import { PetRoutes } from './HTTP/controllers/routes/pet.routes'
 
 export const app = fastify()
 
-app.register(orgRoutes)
-app.register(petRoutes)
+app.register(OrgRoutes)
+app.register(CitiesRoutes)
+app.register(PetRoutes)
