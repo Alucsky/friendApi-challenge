f28efd8e847b641fbb8ea7f66f7fa36cdfa72c44
Alucsky <samuelmolinari8@gmail.com>
2025-03-17 14:17:22 -0300

    controller to search especify pet

---
+++ src/HTTP/controllers/pets/searchEspecifyPet.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { MakeSearchEspecifyPetUseCase } from '@/use-cases/factories/searchEspecifyPetUseCase'
+
+export async function SearchEspecifyPet(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const searchPetSchema = z.object({
+    id: z.string(),
+  })
+
+  const { id } = searchPetSchema.parse(request.params)
+
+  const searchPetUseCase = MakeSearchEspecifyPetUseCase()
+
+  const { pet } = await searchPetUseCase.execute({ pet_Id: id })
+
+  return reply.status(201).send({ pet })
+}
